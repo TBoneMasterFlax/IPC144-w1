dd38e447fb503385a926d9c430c9750d4ae3dbd3
TBoneMasterFlax <87281868+TBoneMasterFlax@users.noreply.github.com>
2021-07-11 17:06:13 -0400

    Add files via upload

---
+++ p1/w1p1.c
@@ -0,0 +1,64 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+#define WORD_LEN 15
+#define VOCAL_LEN 100
+
+struct Vocal
+{
+    char word[WORD_LEN + 1];
+    // How long to hold the note (in seconds)
+    int sustainTime;
+};
+
+struct Song
+{
+    char title[WORD_LEN + 1];
+    char artist[WORD_LEN + 1];
+    struct Vocal lyrics[VOCAL_LEN];
+    int words;
+};
+
+int karaoke(const struct Song* song)
+{
+    int i, tSeconds = 0;
+    struct Vocal* word;
+
+    printf("%.16s (by:%s)\n\n", song->title, song->artist);
+
+    for (i = 0; i < song->words; i++)
+    {
+        word = &song->lyrics[i];
+        printf("%s ", word->word);
+        tSeconds += word->sustainTime;
+
+        if (word->sustainTime == 4)
+        {
+            puts("...");
+        }
+    }
+
+    return tSeconds;
+}
+
+void convertMinSec(const int seconds, int* min, int* sec)
+{
+    *min = seconds % 60;
+    *sec = seconds / 60;
+}
+
+int main(void)
+{
+    // !!! DO NOT MODIFY THE INITIALIZATION OF THE "chorus" VARIABLE !!!
+    struct Song chorus = { "We Are The World", "Various Artists", {
+        {"We", 1}, {"are", 1}, {"the", 1}, {"world", 4}, {"We", 1}, {"are", 1}, {"the", 1}, {"children", 4},
+        {"We", 1}, {"are", 1}, {"the", 1}, {"ones", 1}, {"who", 1}, {"make", 1}, {"a", 1}, {"brighter", 2},
+        {"day", 2}, {"so", 1},{"lets", 1}, {"start", 1}, {"giving", 4} }, 21 };
+
+    int minutes = 0, seconds = 0, totalSeconds = karaoke(&chorus);
+    convertMinSec(totalSeconds, &seconds, &minutes);
+
+    printf("Duration: %02d:%02d\n\n", minutes, seconds);
+
+    return 0;
+}
---
+++ p2/w1p2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void)
+{
+	printf("+-------------------------------+\n");
+	printf("| Introduction to C Programming |\n");
+	printf("+-------------------------------+\n");
+	printf("Program written by:\n");
+	printf("Tuan Nguyen\n");
+	return 0;
+}
